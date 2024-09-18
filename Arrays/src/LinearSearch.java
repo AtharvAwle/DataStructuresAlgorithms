a5739cb995bd9d09d14e124e63ea12c9f51cae34
AtharvAwle <atharvawle0156@gmail.com>
2024-09-18 10:12:16 +0530

    Array basics

---
+++ Arrays/src/LinearSearch.java
@@ -0,0 +1,14 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+
+    }
+
+    public static int linearSearch(int arr[],int key){
+        for (int i = 0 ; i < arr.length ; i++){
+            if (arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
